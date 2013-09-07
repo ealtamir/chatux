ceca704e9a2fb8e47836f1428ae3cf9868e1d2d3
Enzo Altamiranda <enzo.alt@gmail.com>
2013-09-07 18:44:31 -0300

    Created a small program to test pipes.

--- src/pipe_test.c
+++ src/pipe_test.c
@@ -1,18 +1,55 @@
+#include "../lib/common_headers.h"
+#include "../lib/error_functions.h"
 #include <sys/wait.h>
-#include "auxheader.h"
 
-#define BUF_SIZE 10
+#define     BUF_SIZE            10
+#define     INPUT_BUF_SIZE      500
 
 int main(int argc, const char *argv[])
 {
-    int pdf[2];
     char buf[BUF_SIZE];
+    char c = 0;
+    char gen_buf[INPUT_BUF_SIZE];
+    int index = 0;
+    int pfd[2];
     ssize_t numRead;
 
     if (argc != 2 || strcmp(argv[1], "--help") == 0)
         usageErr("%s string\n", argv[0]);
+    else
+        printf("%s\n", argv[1]);
 
     if (pipe(pfd) == -1)
         errExit("pipe");
+
+    switch (fork()) {
+    case -1:
+        errExit("fork");
+    break;
+    case 0: /* child gets its PID set to 0 */
+        if (close(pfd[1]) == -1)
+            errExit("close");
+
+        while(1) {
+            index = read(pfd[0], &gen_buf, INPUT_BUF_SIZE);
+            gen_buf[index] = '\0';
+            printf("Child read: %s\n", gen_buf);
+        }
+
+    break;
+    default: /* parent */
+        if (close(pfd[0]) == -1)
+            errExit("close");
+
+        while(1) {
+            while((gen_buf[index] = getchar()) != '\n' &&
+                    index < INPUT_BUF_SIZE) {
+                write(pfd[1], &gen_buf[index], 1);
+                index++;
+            }
+        }
+    break;
+    }
+
     return 0;
 }
