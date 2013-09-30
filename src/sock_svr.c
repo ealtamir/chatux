a6411a54e2eb89b81096401f2cbcb884d846fc14
Enzo Altamiranda <enzo.alt@gmail.com>
2013-09-30 09:59:06 -0300

    Advancing the chatux thread communication system.

--- src/sock_svr.c
+++ src/sock_svr.c
@@ -1,14 +1,23 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <limits.h>
+#include <fcntl.h>
 
 #include "../lib/common_headers.h"
 #include "../lib/error_functions.h"
 #include "../lib/get_num.h"
 
-#define     PORT_NUM    "50000"
-#define     BACKLOG     5
-#define     ADDRSTRLEN  (NI_MAXHOST + NI_MAXSERV + 10)
+#define     MAX_DATA_SIZE   (1024*4)
+#define     PORT_NUM        "50000"
+#define     BACKLOG         5
+#define     ADDRSTRLEN      (NI_MAXHOST + NI_MAXSERV + 10)
+#define     SERVER_PATHNAME "/tmp/chatux_server"
+
+typedef struct {
+    int msg_size;
+    int pipe_fd;
+} ThreadMsgHeader;
 
 typedef struct {
     int cfd;
@@ -53,7 +62,7 @@ int main(int argc, const char *argv[])
         // Serve request in a new thread.
         delegateRequest(cfd, (struct sockaddr *) &claddr, addrlen);
 
-        close(cfd);
+        //close(cfd);
     }
 
     close(sfd);
@@ -100,13 +109,105 @@ int delegateRequest(int fd, const struct sockaddr *sa, socklen_t salen) {
 
     return 0;
 }
+int readClientData(int cfd, char *data_buffer, int data_len) {
+
+    int index;
+    int val = -1;
+
+    for(index = 0; val != 0 && index < data_len; index++) {
+
+        val = read(cfd, &data_buffer[index], 1);
+
+        if (val == 1) {
+            if (data_buffer[index] == '\n') {
+                data_buffer[index] = '\0';
+                break;
+            }
+        } else if (val == -1) {
+            errMsg("Thread failed in a read operation.");
+            return -1;
+        }
+    }
+
+    return index;
+}
+int getDataFromServer(int *pipe_fd) {
+
+    int val = -1;
+    ThreadMsgHeader t_header;
+
+    val = read(pipe_fd[1], &t_header, sizeof(ThreadMsgHeader));
+    if (val == -1) {
+        errMsg("Error when thread tried to read from pipe");
+        return -1;
+    }
+
+
+}
+
+int sendDataToServer(char *client_data, int data_len, int *pipe_fd) {
+
+    ThreadMsgHeader t_header;
+    int fifo_fd = -1;
+    ssize_t val = -1;
+
+    fifo_fd = open(SERVER_PATHNAME, O_WRONLY);
+    if (fifo_fd == -1) {
+        errMsg("Error when opening server FIFO.");
+        return -1;
+    }
+
+    val = pipe(pipe_fd);
+    if (val == -1) {
+        errMsg("Error when opening thread pipe.");
+        return -1;
+    }
+
+    t_header.msg_size = data_len;
+    t_header.pipe_fd = pipe_fd[1];
+    val = write(fifo_fd, &t_header, sizeof(ThreadMsgHeader));
+    if (val != sizeof(ThreadMsgHeader)) {
+        errMsg("Error when writing header to server FIFO: size mismatch.");
+        return -1;
+    }
+
+    val = write(fifo_fd, client_data, data_len);
+    if (val != data_len) {
+        errMsg("Error when writing client data to server FIFO: size mismatch.");
+        return -1;
+    }
+
+    return 0;
+}
 
 void* toThreadDelegator(void *args) {
 
     ThreadData *td = (ThreadData*) args;
+    char client_data[MAX_DATA_SIZE];
+    int val = -1;
+    int svr_fifo = 0;
+    int pipe_fd[2];
 
     fprintf(stdout, "Thread created - (%s, %s)\n", td->host, td->service);
 
+    val = readClientData(td->cfd, client_data, MAX_DATA_SIZE);
+    if (val == -1)
+        errMsg("Error when reading data from a client");
+
+    val = sendDataToServer(client_data, MAX_DATA_SIZE, pipe_fd);
+    if (val == -1)
+        errMsg("Error while sending data to server.");
+
+    val = getDataFromServer(pipe_fd);
+    if (val != sizeof(ThreadMsgHeader))
+        errMsg("Error when receiving response from server in thread.");
+
+    fprintf(stdout, "read: %s\n", client_data);
+
+    //
+    // Cleaning operations
+    //
+    close(td->cfd);         // Close client socket
     freeThreadData(td);
 
     return NULL;
